437d2152cdebb9861e12a4a294bd295a29884478
Your Name <you@example.com>
2024-04-20 17:09:09 +0100

    implement clerk webhook1

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,31 +1,32 @@
-import mongoose,{Mongoose} from "mongoose";
-import { cache } from "react";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn : Mongoose | null;
-    promise : Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose
 
 if(!cached) {
-    cached =(global as any).mongoose = {
-        conn:null, promise : null
-    }
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connectToDatabase = async() => {
-    if (cached.conn) return cached.conn; // check for connection first , exit with old conn if 1
-    if (!MONGODB_URL) throw new Error('MONGODB_URL not defined');
-    cached.promise= 
-         cached.promise || 
-         mongoose.connect(MONGODB_URL,{
-            dbName: 'Cluster0',bufferCommands:false })// if 0 we make a new connection to mongoDB
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'pfa', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
-
-// Serveless Nature of nextjs 
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes:['api/webhooks/clerk']
+  publicRoutes:['/api/webhooks/clerk']
 });
  
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/(api|trpc)(.*)"],
 };
 
+ 
